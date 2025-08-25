ac2c4a29ca249d6a09f97cade0d6195558880f88
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-08-25 14:45:36 +0300

    update project

---
+++ _nuxt/Cv7guTWW.js
@@ -0,0 +1,1 @@
+import{_ as r,c as s,o,a2 as t}from"./dB5ScyD7.js";const n={};function a(e,c){return o(),s("tr",null,[t(e.$slots,"default")])}const l=Object.assign(r(n,[["render",a]]),{__name:"ProseTr"});export{l as default};
